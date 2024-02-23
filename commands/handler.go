fc3c96a6ef3349f064ada0a837ca51806a9bc897
abdulrasheedlawal <lawalabdulrasheedbolaji@gmail.com>
2024-02-23 21:50:26 +0100

    feat: add support for hset and hget

--- commands/handler.go
+++ commands/handler.go
@@ -17,10 +17,12 @@ var Handlers = map[string]func([]parser.Value) parser.Value{
 	"PING": ping,
 	"SET":  set,
 	"GET":  get,
+	"HGET": hget,
+	"HSET": hset,
 }
 
-var setsHashmap = map[string]string{}
-var setsMtx = sync.RWMutex{}
+var regularStore = map[string]string{}
+var regStoreMtx = sync.RWMutex{}
 
 func set(args []parser.Value) parser.Value {
 	if len(args) != 2 {
@@ -30,9 +32,9 @@ func set(args []parser.Value) parser.Value {
 	key := args[0].Bulk
 	val := args[1].Bulk
 
-	setsMtx.Lock()
-	setsHashmap[key] = val
-	setsMtx.Unlock()
+	regStoreMtx.Lock()
+	regularStore[key] = val
+	regStoreMtx.Unlock()
 
 	return parser.Value{Typ: parser.SIMPLE_STRING, Str: "OK"}
 }
@@ -44,12 +46,55 @@ func get(args []parser.Value) parser.Value {
 
 	key := args[0].Bulk
 
-	setsMtx.RLock()
-	value, ok := setsHashmap[key]
-	setsMtx.RUnlock()
+	regStoreMtx.RLock()
+	value, ok := regularStore[key]
+	regStoreMtx.RUnlock()
 
 	if !ok {
 		return parser.Value{Typ: parser.NULL}
 	}
 	return parser.Value{Typ: parser.BULK_STRING, Bulk: value}
 }
+
+var hashStore = map[string]map[string]string{}
+var hashStoreMtx = sync.RWMutex{}
+
+func hset(args []parser.Value) parser.Value {
+	if len(args) != 3 { // hashmap, key, val
+		return parser.Value{Typ: parser.SIMPLE_ERROR, Str: "ERR wrong number of arguments for 'hset' command"}
+	}
+
+	hash := args[0].Bulk
+	key := args[1].Bulk
+	val := args[2].Bulk
+
+	hashStoreMtx.Lock()
+	if _, ok := hashStore[hash]; !ok {
+		hashStore[hash] = map[string]string{}
+	}
+	hashStore[hash][key] = val
+	hashStoreMtx.Unlock()
+
+	return parser.Value{Typ: parser.BULK_STRING, Bulk: "OK"}
+}
+
+func hget(args []parser.Value) parser.Value {
+	if len(args) != 2 {
+		return parser.Value{Typ: parser.SIMPLE_ERROR, Str: "ERR wrong number of arguments for 'hget' command"}
+	}
+
+	hash := args[0].Bulk
+	key := args[1].Bulk
+
+	hashStoreMtx.RLock()
+	val, ok := hashStore[hash][key]
+	hashStoreMtx.RUnlock()
+
+	if !ok {
+		return parser.Value{Typ: parser.NULL}
+	}
+	return parser.Value{Typ: parser.BULK_STRING, Bulk: val}
+}
+
+// TODO: implement hgetAll
+// func hgetAll(args []parser.Value) parser.Value {}
